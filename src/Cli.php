3e20167ccaecd6ee89db1a00b772083bd47cef88
bl1ndy <ayoblindstone@gmail.com>
2021-02-03 13:18:43 +0300

    'add brain-even game'

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,11 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greetings()
+function getUserName()
 {
     line("Welcome to the Brain Game!\n");
     $name = prompt('May I have your name?');
     line("\nHello, %s!", $name);
+
+    return $name;
 }
